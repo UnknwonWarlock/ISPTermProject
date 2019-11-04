6a2c690bf09c28a5e1b2373081c40b676ba1c02c
mysticAxolotl <brighid1681@yahoo.com>
2019-11-04 12:53:37 -0500

    login working, still  needs to keep username tho

--- login.php
+++ login.php
@@ -5,15 +5,57 @@
         <link rel="stylesheet" type="text/css" href="assets/styles/homeStyle.css">
     </head>
     <body>
-        <header><a class="home" href="home.php">DigiScrap</a>: A Place for Scrap Bookers!</header>
+        <header><a class="home" href="login.php">DigiScrap</a>: A Place for Scrap Bookers!</header>
         <ul>
             <li><a class="active" href="#">Login</a></li>
             <li><a href="about.html">About</a></li>
-        </ul>
-        <form class="login" action="main.php" method="POST"> 
+        </ul>        
+        <?php
+            $error = "";
+            if( isset( $_POST["submit"] ))
+            {
+                
+                $MySQL_db = "db1.cs.uakron.edu:3306"; 
+                $MySQL_username = "mcr66";
+                $MySQL_password = "ohl5eiB0";
+
+                $db = mysqli_connect($MySQL_db, $MySQL_username, $MySQL_password);
+                if(!$db) {
+
+                    print "Error - Could not connect to MySQL";
+                    exit;
+                }
+
+                // Select schema from database
+                $error = mysqli_select_db($db, "ISP_" . $MySQL_username);
+                if (!$error) {
+
+                    print "Error - Could not select the database";
+                    exit;
+                }
+                
+                $query = "SELECT * FROM Users WHERE " .
+                         "Username='" . $_POST['user'] . "' " .
+                         "AND Pass='" . $_POST['pass'] . "'";
+                $result = mysqli_query($db, $query);
+                mysqli_close($db);
+                if( mysqli_num_rows($result) > 0 )
+                {
+                    header("Location: main.php");
+                }
+                $error = "Username or Password incorrect <br>";
+            }
+        ?>
+        <form class="login" action="login.php" method="POST"> 
             Please enter your username and password!<br>
             <input type="text" name="user" placeholder="Username" required><br>
             <input type="password" name="pass" placeholder="Password" required><br>
+            <?php
+                if( isset( $_POST["submit"] ) )
+                {
+                    print "invalid username or password <br>";
+                }
+            ?>
             <input type="submit" name="submit" value="Submit">
         </form>
     </body>
